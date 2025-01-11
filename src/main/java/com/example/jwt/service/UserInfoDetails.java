b2429b3612260332b89104c2ce5540ab5916b55e
badr cherhoune <badrcherhoune199@gmail.com>
2025-01-11 12:16:22 +0100

    application complet developed en java Spring boot 3 JWT

---
+++ src/main/java/com/example/jwt/service/UserInfoDetails.java
@@ -0,0 +1,61 @@
+package com.example.jwt.service;
+
+import com.example.jwt.entity.UserInfo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoDetails implements UserDetails {
+
+    private String username; // Changed from 'name' to 'username' for clarity
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(UserInfo userInfo) {
+        this.username = userInfo.getName(); // Assuming 'name' is used as 'username'
+        this.password = userInfo.getPassword();
+        this.authorities = List.of(userInfo.getRoles().split(","))
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public String getUsername() {
+        return username ;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
